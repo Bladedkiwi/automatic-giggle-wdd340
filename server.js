f72d61c3469ee1f113bd5b372e7f4b8d1637f7b9
Bladedkiwi <bladedkiwi@byui.edu>
2024-10-24 13:18:50 -0500

    Views Added/Working

--- server.js
+++ server.js
@@ -5,15 +5,25 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const expressLayouts = require('express-ejs-layouts')
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.set("view engine", "ejs")
+    .use(expressLayouts)
+    .set('layout', './layouts/layout')
+    .use(require('./routes/static'))
+    /**
+     * Base Route
+     * <p>render() retrieves the specified view which is index in this case and sends it back to the browser</p>
+     * <p>{title: "Home"} Treated like a variable with supplies the value that the "head" partial file expects to receive. This gets passed to the view</p>
+     */
+    .get('/', (req, res) => {
+      res.render('index', {title: "Home"})})
 
 /* ***********************
  * Local Server Information
@@ -26,5 +36,5 @@ const host = process.env.HOST
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+    console.log(`app listening on ${host}:${port}`)
 })
